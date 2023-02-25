f4964b66b52d6728661d6e92d0047bce9ab69f24
sailesh31 <saileshkalyanapu@gmail.com>
2023-02-25 21:20:02 +0530

    Init App Again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myApp';
-  loaderPersonInfo = true;
+  loaderPersonInfo : any;
+  interval : any;
+
+  startTime() {
+    this.interval = setTimeout(() => {this.loaderPersonInfo = false;
+    }, 5000);
+    
+  }
+ 
+
+  stopTime() {
+    clearInterval(this.interval);
+  }
+
+  ngOnInit(){
+    this.loaderPersonInfo = true;
+    this.startTime();
+  }
 }
